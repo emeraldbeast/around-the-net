9625a6a0ad96b94a205685ec83509886fb78c912
Dhananjay <f20240506@goa.bits-pilani.ac.in>
2025-10-16 11:43:52 +0530

    Added a team database, now ready for dynamic updating of teams

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const port = 3000;
 const jwt = require('jsonwebtoken');
 const cookieParser = require('cookie-parser');
 const bcrypt = require('bcrypt');
+const teams = require('./teams'); 
 // const mongoose = require("mongoose");
 // const User = require('./modules/user');
 
@@ -20,5 +21,23 @@ app.get('/',(req,res)=>{
 app.get('/fixtures',(req,res)=>{
     res.render('fixtures')
 })
+
+app.get('/teams/:id', (req, res) => {
+  const teamId = (req.params.id);
+  const team = teams.find(t => t.id === teamId);
+  if (!team) {
+    return res.status(404).render('404', {
+      message: "Team not found. Please check the ID."
+    });
+  }
+  res.render('team-detail', { team });
+});
+
+app.use((req, res) => {
+  res.status(404).render('404', {
+    message: "Oops! The page you're looking for doesn't exist."
+  });
+});
+
 
 app.listen(port);
---
+++ teams.js
@@ -0,0 +1,20 @@
+const teams = [
+  {
+    id: "ahmedabad-sg-pipers",
+    name: "Ahmedabad SG Pipers",
+    logo: "https://ultimatetabletennis.in/_next/image?url=https%3A%2F%2Futtcms.tinglabs.in%2Fuploads%2FAhmedabad_SG_Pipers_6d67464c1f.png&w=640&q=75",
+    owner: "Shrivats Prabhu",
+    captain: "Anvesh Mishra", 
+    players: ["Aarush", "Vedang", "Abhinav", "Goyal", "Chinhalli", "Padhi"]
+  },
+  {
+    id: "dabang-delhi-ttc",
+    name: "Dabang Delhi T.T.C",
+    logo: "https://ultimatetabletennis.in/_next/image?url=https%3A%2F%2Futtcms.tinglabs.in%2Fuploads%2FDabang_Delhi_T_T_C_f65b845603.png&w=640&q=75",
+    owner: "Ayush Shukla",
+    captain: "Arya Jain",
+    players: ["Divyam", "Aayush", "Goatnaath", "Jain","Sigmawat","Keshav"]
+  }
+];
+
+module.exports = teams;
