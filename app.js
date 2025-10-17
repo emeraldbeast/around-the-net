d0b1b66b61cba1fbdbbbb1e73d4788f61213cd10
Dhananjay <f20240506@goa.bits-pilani.ac.in>
2025-10-17 09:21:41 +0530

    Trying for a Vercel Deployment

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.port||3000;
 const jwt = require('jsonwebtoken');
 const cookieParser = require('cookie-parser');
 const bcrypt = require('bcrypt');
