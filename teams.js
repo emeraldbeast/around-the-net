9fe27ff0466f0bbcea5e86acf711d163d20b862b
Dhananjay <f20240506@goa.bits-pilani.ac.in>
2025-10-18 14:24:26 +0530

    Just some more changes, planning to add Live Auction Feature

--- teams.js
+++ teams.js
@@ -14,6 +14,54 @@ const teams = [
     owner: "Ayush Shukla",
     captain: "Arya Jain",
     players: ["Divyam", "Aayush", "Goatnaath", "Jain","Sigmawat","Keshav"]
+  },
+  {
+    id: "dempo-goa-challengers",
+    name: "Dempo Goa Challengers",
+    logo: "https://ultimatetabletennis.in/_next/image?url=https%3A%2F%2Futtcms.tinglabs.in%2Fuploads%2FDempo_Goa_Challengers_2_4f4ca14b88.png&w=640&q=75",
+    owner: "Shrivats Prabhu",
+    captain: "Anvesh Mishra", 
+    players: ["Aarush", "Vedang", "Abhinav", "Goyal", "Chinhalli", "Padhi"]
+  },
+  {
+    id: "jaipur-patriots",
+    name: "Jaipur Patriots",
+    logo: "https://ultimatetabletennis.in/_next/image?url=https%3A%2F%2Futtcms.tinglabs.in%2Fuploads%2FJaipur_Patriots_a2d2ae68e2.png&w=640&q=75",
+    owner: "Ayush Shukla",
+    captain: "Arya Jain",
+    players: ["Divyam", "Aayush", "Goatnaath", "Jain","Sigmawat","Keshav"]
+  },
+  {
+    id: "kolkata-thunder-blades",
+    name: "Kolkata Thunder Blades",
+    logo: "https://ultimatetabletennis.in/_next/image?url=https%3A%2F%2Futtcms.tinglabs.in%2Fuploads%2FKolkata_Thunder_Blades_779f7cc584.png&w=640&q=75",
+    owner: "Shrivats Prabhu",
+    captain: "Anvesh Mishra", 
+    players: ["Aarush", "Vedang", "Abhinav", "Goyal", "Chinhalli", "Padhi"]
+  },
+  {
+    id: "pbg-pune-jaguars",
+    name: "PBG Pune Jaguars",
+    logo: "https://ultimatetabletennis.in/_next/image?url=https%3A%2F%2Futtcms.tinglabs.in%2Fuploads%2FPune_Jaguars_0f95943b31.png&w=640&q=75",
+    owner: "Ayush Shukla",
+    captain: "Arya Jain",
+    players: ["Divyam", "Aayush", "Goatnaath", "Jain","Sigmawat","Keshav"]
+  },
+  {
+    id: "stanleys-chennai-lions",
+    name: "Ahmedabad SG Pipers",
+    logo: "https://ultimatetabletennis.in/_next/image?url=https%3A%2F%2Futtcms.tinglabs.in%2Fuploads%2Fstanleys_chennai_lion_white_with_stroke_391771c2d6.png&w=640&q=75",
+    owner: "Shrivats Prabhu",
+    captain: "Anvesh Mishra", 
+    players: ["Aarush", "Vedang", "Abhinav", "Goyal", "Chinhalli", "Padhi"]
+  },
+  {
+    id: "u-mumba-tt",
+    name: "U Mumba T.T",
+    logo: "https://ultimatetabletennis.in/_next/image?url=https%3A%2F%2Futtcms.tinglabs.in%2Fuploads%2FU_Mumba_TT_e31b087694.png&w=640&q=75",
+    owner: "Ayush Shukla",
+    captain: "Arya Jain",
+    players: ["Divyam", "Aayush", "Goatnaath", "Jain","Sigmawat","Keshav"]
   }
 ];
 
