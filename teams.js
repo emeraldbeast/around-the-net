1fa4bfdcdb7653b355aa188b0f2b04b0524a51e8
Dhananjay <f20240506@goa.bits-pilani.ac.in>
2025-10-28 12:20:33 +0530

    Added more teams to the website, and also removed the default details, now every team is customised

--- teams.js
+++ teams.js
@@ -3,65 +3,65 @@ const teams = [
     id: "backhand-blitzers",
     name: "Backhand Blitzers",
     logo: "/logos/bac-bli.png",
-    owner: "Shrivats Prabhu",
-    captain: "Anvesh Mishra", 
-    players: ["Aarush", "Vedang", "Abhinav", "Goyal", "Chinhalli", "Padhi"]
+    owner: "Dhananjay Bhattacharya",
+    captain: "TBA", 
+    // players: ["Aarush", "Vedang", "Abhinav", "Goyal", "Chinhalli", "Padhi"]
   },
   {
     id: "noughty-netters",
     name: "Noughty Netters",
     logo: "/logos/nou-net.png",
-    owner: "Ayush Shukla",
-    captain: "Arya Jain",
-    players: ["Divyam", "Aayush", "Goatnaath", "Jain","Sigmawat","Keshav"]
+    owner: "Aadinath Keshav",
+    captain: "TBA",
+    // players: ["Divyam", "Aayush", "Goatnaath", "Jain","Sigmawat","Keshav"]
   },
   {
     id: "the-batmen",
     name: "The Batmen",
     logo: "/logos/the-bat.png",
-    owner: "Shrivats Prabhu",
-    captain: "Anvesh Mishra", 
-    players: ["Aarush", "Vedang", "Abhinav", "Goyal", "Chinhalli", "Padhi"]
+    owner: "Rohit Raj",
+    captain: "TBA", 
+    // players: ["Aarush", "Vedang", "Abhinav", "Goyal", "Chinhalli", "Padhi"]
   },
   {
     id: "the-untouchables",
     name: "The Untouchables",
     logo: "/logos/the-unt.png",
+    owner: "Akshat Pandey",
+    captain: "TBA",
+    // players: ["Divyam", "Aayush", "Goatnaath", "Jain","Sigmawat","Keshav"]
+  },
+  {
+    id: "pong-stars",
+    name: "Pong Stars",
+    logo: "/logos/pong-stars.png",
     owner: "Ayush Shukla",
-    captain: "Arya Jain",
-    players: ["Divyam", "Aayush", "Goatnaath", "Jain","Sigmawat","Keshav"]
+    captain: "TBA", 
+    // players: ["Aarush", "Vedang", "Abhinav", "Goyal", "Chinhalli", "Padhi"]
   },
   {
-    id: "kolkata-thunder-blades",
-    name: "Kolkata Thunder Blades",
-    logo: "https://ultimatetabletennis.in/_next/image?url=https%3A%2F%2Futtcms.tinglabs.in%2Fuploads%2FKolkata_Thunder_Blades_779f7cc584.png&w=640&q=75",
-    owner: "Shrivats Prabhu",
-    captain: "Anvesh Mishra", 
-    players: ["Aarush", "Vedang", "Abhinav", "Goyal", "Chinhalli", "Padhi"]
+    id: "ball-breakers",
+    name: "Ball Breakers",
+    logo: "/logos/bal-bre.png",
+    owner: "Priyanshu Jain",
+    captain: "TBA",
+    // players: ["Divyam", "Aayush", "Goatnaath", "Jain","Sigmawat","Keshav"]
   },
   {
-    id: "pbg-pune-jaguars",
-    name: "PBG Pune Jaguars",
-    logo: "https://ultimatetabletennis.in/_next/image?url=https%3A%2F%2Futtcms.tinglabs.in%2Fuploads%2FPune_Jaguars_0f95943b31.png&w=640&q=75",
-    owner: "Ayush Shukla",
-    captain: "Arya Jain",
-    players: ["Divyam", "Aayush", "Goatnaath", "Jain","Sigmawat","Keshav"]
-  },
-  {
-    id: "stanleys-chennai-lions",
+    id: "high-rankers",
     name: "Ahmedabad SG Pipers",
-    logo: "https://ultimatetabletennis.in/_next/image?url=https%3A%2F%2Futtcms.tinglabs.in%2Fuploads%2Fstanleys_chennai_lion_white_with_stroke_391771c2d6.png&w=640&q=75",
-    owner: "Shrivats Prabhu",
-    captain: "Anvesh Mishra", 
-    players: ["Aarush", "Vedang", "Abhinav", "Goyal", "Chinhalli", "Padhi"]
+    logo: "/logos/hig-ran.png",
+    owner: "Arihant Bansal",
+    captain: "TBA", 
+    // players: ["Aarush", "Vedang", "Abhinav", "Goyal", "Chinhalli", "Padhi"]
   },
   {
-    id: "u-mumba-tt",
-    name: "U Mumba T.T",
-    logo: "https://ultimatetabletennis.in/_next/image?url=https%3A%2F%2Futtcms.tinglabs.in%2Fuploads%2FU_Mumba_TT_e31b087694.png&w=640&q=75",
-    owner: "Ayush Shukla",
-    captain: "Arya Jain",
-    players: ["Divyam", "Aayush", "Goatnaath", "Jain","Sigmawat","Keshav"]
+    id: "sancoale-smashers",
+    name: "Sancoale Smashers",
+    logo: "/logos/san-sma.png",
+    owner: "Vismay V Shanbhag",
+    captain: "TBA",
+    // players: ["Divyam", "Aayush", "Goatnaath", "Jain","Sigmawat","Keshav"]
   }
 ];
 
