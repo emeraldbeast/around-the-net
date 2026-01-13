a658bd665099e527f2ae90e316713bd599c7b24d
Dhananjay <f20240506@goa.bits-pilani.ac.in>
2026-01-13 19:26:34 +0530

    D

--- matchdata.js
+++ matchdata.js
@@ -85,9 +85,9 @@ const matches = [
         game: "Singles C",
         playerA: "Divyam",
         playerB: "Prateek",
-        games: ["4-11", "-", "-"],//When Match starts, replace Scheduled with - - -, and then keep adding scores
-        winner: null,
-        status: "Ongoing"//Change this to Ongoing, when game starts, then change to Completed when game over
+        games: ["4-11", "10-12", "-"],//When Match starts, replace Scheduled with - - -, and then keep adding scores
+        winner: "Prateek",
+        status: "Completed"//Change this to Ongoing, when game starts, then change to Completed when game over
       },
       {
         game: "Doubles",
@@ -95,7 +95,7 @@ const matches = [
         playerB: "Aayush & Pranjal",
         games: ["-", "-", "-"],//When Match starts, replace Scheduled with - - -, and then keep adding scores
         winner: null,
-        status: "Upcoming"//Change this to Ongoing, when game starts, then change to Completed when game over
+        status: "Ongoing"//Change this to Ongoing, when game starts, then change to Completed when game over
       },
       {
         game: "Singles D",
