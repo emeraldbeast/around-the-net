e6e9458532b769affca82d888a658c29d7b32eb1
Dhananjay <f20240506@goa.bits-pilani.ac.in>
2026-01-15 21:30:19 +0530

    B

--- matchdata.js
+++ matchdata.js
@@ -209,7 +209,7 @@ const matches = [
         game: "Doubles",
         playerA: "Omkar Vanjari & Aadinath Keshav",
         playerB: "Aditya Ambrish & Vedaant Desai",
-        games: ["-", "-", "-"],//When Match starts, replace Scheduled with - - -, and then keep adding scores
+        games: ["11-9", "-", "-"],//When Match starts, replace Scheduled with - - -, and then keep adding scores
         winner: null,
         status: "Ongoing"//Change this to Ongoing, when game starts, then change to Completed when game over
       },
