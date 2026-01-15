1986ed1ee9d8aea6b4e5f00597841051a8880c80
Dhananjay <f20240506@goa.bits-pilani.ac.in>
2026-01-15 22:07:07 +0530

    WInner added

--- matchdata.js
+++ matchdata.js
@@ -226,8 +226,8 @@ const matches = [
         playerA: "Khushi Mishra",
         playerB: "Daksh Diwakar",
         games: ["11-8", "11-7", "-"],//When Match starts, replace Scheduled with - - -, and then keep adding scores
-        winner: null,
-        status: "Ongoing"//Change this to Ongoing, when game starts, then change to Completed when game over
+        winner: "Khushi Mishra",
+        status: "Completed"//Change this to Ongoing, when game starts, then change to Completed when game over
       }
     ]
   },
