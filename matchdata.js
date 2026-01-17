47c1aad92e05abdab985a0576d1053a7dc3ff9d6
Dhananjay <f20240506@goa.bits-pilani.ac.in>
2026-01-17 15:14:18 +0530

    Match 6 readt to start

--- matchdata.js
+++ matchdata.js
@@ -331,16 +331,16 @@ const matches = [
       },
       {
         game: "Singles D",
-        playerA: "TBA",
-        playerB: "TBA",
+        playerA: "Nachiket Khandekar",
+        playerB: "Anvesh Mishra",
         games: ["-", "-", "-"],//When Match starts, replace Scheduled with - - -, and then keep adding scores
         winner: null,
         status: "Upcoming"//Change this to Ongoing, when game starts, then change to Completed when game over
       },
       {
         game: "Singles E",
-        playerA: "TBA",
-        playerB: "TBA",
+        playerA: "Shreyash Narayanan",
+        playerB: "Aditya Ambrish",
         games: ["-", "-", "-"],//When Match starts, replace Scheduled with - - -, and then keep adding scores
         winner: null,
         status: "Upcoming"//Change this to Ongoing, when game starts, then change to Completed when game over
