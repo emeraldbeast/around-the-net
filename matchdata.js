28a12561ef922e21b2656a40974beba17042f6e8
Dhananjay <f20240506@goa.bits-pilani.ac.in>
2026-01-17 15:22:52 +0530

    A

--- matchdata.js
+++ matchdata.js
@@ -301,9 +301,9 @@ const matches = [
         game: "Singles A",
         playerA: "Nachiket Khandekar",
         playerB: "Aditya Ambrish",
-        games: ["11-3", "-", "-"],//When Match starts, replace Scheduled with - - -, and then keep adding scores
-        winner: null,
-        status: "Ongoing"//Change this to Ongoing, when game starts, then change to Completed when game over
+        games: ["11-3", "11-3", "-"],//When Match starts, replace Scheduled with - - -, and then keep adding scores
+        winner: "Nachiket Khandekar",
+        status: "Completed"//Change this to Ongoing, when game starts, then change to Completed when game over
       },
       {
         game: "Singles B",
@@ -311,7 +311,7 @@ const matches = [
         playerB: "Anvesh Mishra",
         games: ["-", "-", "-"],//When Match starts, replace Scheduled with - - -, and then keep adding scores
         winner: null,
-        status: "Upcoming"//Change this to Ongoing, when game starts, then change to Completed when game over
+        status: "Ongoing"//Change this to Ongoing, when game starts, then change to Completed when game over
       },
       {
         game: "Singles C",
