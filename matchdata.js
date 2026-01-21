12e13e41e9fcd9c358da4f684ea7c1d08d0f0419
Dhananjay <f20240506@goa.bits-pilani.ac.in>
2026-01-21 22:04:43 +0530

    B

--- matchdata.js
+++ matchdata.js
@@ -499,7 +499,7 @@ const matches = [
         game: "Doubles",
         playerA: "Veer Kanakia & Jatin Vasandani",
         playerB: "Divy Jakhotiya & Aarush Nabhar",
-        games: ["-", "-", "-"],//When Match starts, replace Scheduled with - - -, and then keep adding scores
+        games: ["11-9", "-", "-"],//When Match starts, replace Scheduled with - - -, and then keep adding scores
         winner: null,
         status: "Ongoing"//Change this to Ongoing, when game starts, then change to Completed when game over
       },
