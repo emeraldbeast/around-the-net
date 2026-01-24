cfb955472035cc244768123aec8c662090072f20
Dhananjay <f20240506@goa.bits-pilani.ac.in>
2026-01-24 16:36:31 +0530

    adding match 10 to github

--- matchdata.js
+++ matchdata.js
@@ -520,6 +520,64 @@ const matches = [
         status: "Did Not Play"//Change this to Ongoing, when game starts, then change to Completed when game over
       }
     ]
+  },
+  {
+    id: "match-10",
+    date:"25th January",
+    teamA: "The Batmen",
+    teamB: "Blade Runners",
+    score: "1-4",//Update this at last, when all scores are done
+    status: "Past",//Change this to Live when match starts, and Past when match end
+    submatches: [
+      {
+        game: "Singles A",
+        playerA: "TBA",
+        playerB: "TBA",
+        games: ["-", "-", "-"],//When Match starts, replace Scheduled with - - -, and then keep adding scores
+        winner: null,
+        status: "Upcoming"//Change this to Ongoing, when game starts, then change to Completed when game ove
+      },
+      {
+        game: "Singles B",
+        playerA: "TBA",
+        playerB: "TBA",
+        games: ["-", "-", "-"],//When Match starts, replace Scheduled with - - -, and then keep adding scores
+        winner: null,
+        status: "Upcoming"//Change this to Ongoing, when game starts, then change to Completed when game ove
+      },
+      {
+        game: "Singles C",
+        playerA: "TBA",
+        playerB: "TBA",
+        games: ["-", "-", "-"],//When Match starts, replace Scheduled with - - -, and then keep adding scores
+        winner: null,
+        status: "Upcoming"//Change this to Ongoing, when game starts, then change to Completed when game ove
+      },
+      {
+        game: "Doubles",
+        playerA: "TBA",
+        playerB: "TBA",
+        games: ["-", "-", "-"],//When Match starts, replace Scheduled with - - -, and then keep adding scores
+        winner: null,
+        status: "Upcoming"//Change this to Ongoing, when game starts, then change to Completed when game over
+      },
+      {
+        game: "Singles D",
+        playerA: "TBA",
+        playerB: "TBA",
+        games: ["-", "-", "-"],//When Match starts, replace Scheduled with - - -, and then keep adding scores
+        winner: null,
+        status: "Upcoming"//Change this to Ongoing, when game starts, then change to Completed when game ove
+      },
+      {
+        game: "Singles E",
+        playerA: "TBA",
+        playerB: "TBA",
+        games: ["-", "-", "-"],//When Match starts, replace Scheduled with - - -, and then keep adding scores
+        winner: null,
+        status: "Upcoming"//Change this to Ongoing, when game starts, then change to Completed when game over
+      }
+    ]
   }
 
 ];
