a7e05c213fdb02501b790be597b2acd5d5308eaa
Dhananjay <f20240506@goa.bits-pilani.ac.in>
2026-01-28 19:54:00 +0530

    A

--- matchdata.js
+++ matchdata.js
@@ -591,7 +591,7 @@ const matches = [
         game: "Singles A",
         playerA: "Siddhant Bhutani",
         playerB: "Arya Jain",
-        games: ["-", "-", "-"],//When Match starts, replace Scheduled with - - -, and then keep adding scores
+        games: ["4-11", "-", "-"],//When Match starts, replace Scheduled with - - -, and then keep adding scores
         winner: null,
         status: "Ongoing"//Change this to Ongoing, when game starts, then change to Completed when game over
       },
