9c96ae749ec7ad7181a9993256b1f9a30e95d428
Dhananjay <f20240506@goa.bits-pilani.ac.in>
2026-02-04 22:43:02 +0530

    A

--- matchdata.js
+++ matchdata.js
@@ -816,8 +816,8 @@ const matches = [
     date:"4th February",
     teamA: "The Batmen",
     teamB: "High Rankers",
-    score: "0-0",//Update this at last, when all scores are done
-    status: "Live",//Change this to Live when match starts, and Past when match end
+    score: "1-4",//Update this at last, when all scores are done
+    status: "Past",//Change this to Live when match starts, and Past when match end
     submatches: [
       {
         game: "Singles A",
