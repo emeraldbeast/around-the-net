854037bf028345c2db142de9eb2e5b005564ad90
Dhananjay <f20240506@goa.bits-pilani.ac.in>
2026-02-04 21:35:30 +0530

    A

--- matchdata.js
+++ matchdata.js
@@ -825,7 +825,7 @@ const matches = [
         playerB: "Hriday Sama",
         games: ["-", "-", "-"],//When Match starts, replace Scheduled with - - -, and then keep adding scores
         winner: null,
-        status: "Ongoing"//Change this to Ongoing, when game starts, then change to Completed when game over
+        status: "Upcoming"//Change this to Ongoing, when game starts, then change to Completed when game over
       },
       {
         game: "Singles B",
@@ -841,7 +841,7 @@ const matches = [
         playerB: "Khushi Mishra",
         games: ["-", "-", "-"],//When Match starts, replace Scheduled with - - -, and then keep adding scores
         winner: null,
-        status: "Upcoming"//Change this to Ongoing, when game starts, then change to Completed when game over
+        status: "Ongoing"//Change this to Ongoing, when game starts, then change to Completed when game over
       },
       {
         game: "Doubles",
