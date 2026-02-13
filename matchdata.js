b231910a7ab8e928461fb2f6b3bed6d2c9107eb7
Dhananjay <f20240506@goa.bits-pilani.ac.in>
2026-02-13 21:37:23 +0530

    A

--- matchdata.js
+++ matchdata.js
@@ -963,7 +963,7 @@ const matches = [
         game: "Doubles",
         playerA: "Pranjal Gangwani & Samar Suraj Nayak",
         playerB: "Ashwin Gupta & Vismay Vinayak Shanbhag",
-        games: ["-", "-", "-"],//When Match starts, replace Scheduled with - - -, and then keep adding scores
+        games: ["11-6", "-", "-"],//When Match starts, replace Scheduled with - - -, and then keep adding scores
         winner: null,
         status: "Ongoing"//Change this to Ongoing, when game starts, then change to Completed when game over
       },
