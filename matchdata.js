6b42b0a016dd6351942e3f54e9d678cff3f7587c
mrgreenapple24 <mrchill2405@gmail.com>
2026-04-13 21:36:16 +0530

    Elim 1 Over

--- matchdata.js
+++ matchdata.js
@@ -1203,7 +1203,7 @@ const matches = [
         game: "Singles D",
         playerA: "Khushi Mishra",
         playerB: "Ammar Abdul Azeez",
-        games: ["5-11", "-", "-"], //When Match starts, replace Scheduled with - - -, and then keep adding scores
+        games: ["5-11", "5-11", "-"], //When Match starts, replace Scheduled with - - -, and then keep adding scores
         winner: null,
         status: "Ongoing", //Change this to Ongoing, when game starts, then change to Completed when game over
       },
