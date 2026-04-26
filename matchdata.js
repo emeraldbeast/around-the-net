51cb375e8e2fbafe4d4ca04caedfaae0aadf3e1c
mrgreenapple24 <mrchill2405@gmail.com>
2026-04-26 16:54:46 +0530

    update matchdataa.js

--- matchdata.js
+++ matchdata.js
@@ -1435,7 +1435,7 @@ const matches = [
         game: "Singles D",
         playerA: "Nachiket Khandekar",
         playerB: "Mihir Kulkarni",
-        games: ["-", "-", "-", "-", "-"], //When Match starts, replace Scheduled with - - -, and then keep adding scores
+        games: ["8-11", "-", "-", "-", "-"], //When Match starts, replace Scheduled with - - -, and then keep adding scores
         winner: null,
         status: "Ongoing", //Change this to Ongoing, when game starts, then change to Completed when game over
       },
